792854c02dc7b4e6369d582c086cb9808a7e9874
Johannes Klauss <>
2019-01-15 12:43:48 +0100

    init project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import hotkeys, {HotkeysEvent} from 'hotkeys-js';
+import {useEffect} from "react";
+
+export default function useHotKeys(keys: string, callback: (event: KeyboardEvent, handler: HotkeysEvent) => void) {
+  useEffect(() => {
+    hotkeys(keys, (event, handler) => callback(event, handler));
+
+    return function cleanUp() {
+      
+    }
+  });
+}
