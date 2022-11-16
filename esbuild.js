27752d2522e968ae737f10726501c58667b1242d
Johannes Klauss <klauss.johannes@gmail.com>
2022-11-16 17:09:29 +0100

    split bundling into cjs and esm

--- esbuild.js
+++ esbuild.js
@@ -2,14 +2,28 @@ const esbuild = require('esbuild')
 
 esbuild
   .build({
-    entryPoints: ['./src/index.ts'],
-    outdir: 'dist',
-    bundle: true,
-    sourcemap: true,
-    minify: true,
-    splitting: false,
-    format: 'cjs',
-    target: ['esnext'],
-    external: ['react', 'react-dom'],
+      entryPoints: ['./src/index.ts'],
+      outfile: 'dist/index.cjs',
+      bundle: true,
+      sourcemap: true,
+      minify: false,
+      splitting: false,
+      format: 'cjs',
+      target: ['esnext'],
+      external: ['react', 'react-dom', 'lodash'],
+  })
+  .catch(() => process.exit(1))
+
+esbuild
+  .build({
+      entryPoints: ['./src/index.ts'],
+      outfile: 'dist/index.mjs',
+      bundle: true,
+      sourcemap: true,
+      minify: false,
+      splitting: false,
+      format: 'esm',
+      target: ['esnext'],
+      external: ['react', 'react-dom', 'lodash'],
   })
   .catch(() => process.exit(1))
