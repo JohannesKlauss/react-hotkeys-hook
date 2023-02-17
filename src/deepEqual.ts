6a4f424026fa66391d1a38958c232d8622e68ff6
Johannes Klauss <klauss.johannes@gmail.com>
2023-02-17 17:06:14 +0100

    Fix deepEqual

--- src/deepEqual.ts
+++ src/deepEqual.ts
@@ -1,8 +1,8 @@
 export default function deepEqual(x: any, y: any): boolean {
+  //@ts-ignore
   return x && y && typeof x === 'object' && typeof y === 'object'
     ? Object.keys(x).length === Object.keys(y).length &&
-        Object.keys(x).reduce(function (isEqual, key) {
-          return isEqual && deepEqual(x[key], y[key])
-        }, true)
+        //@ts-ignore
+        Object.keys(x).reduce((isEqual, key) => isEqual && deepEqual(x[key], y[key]), true)
     : x === y
 }
