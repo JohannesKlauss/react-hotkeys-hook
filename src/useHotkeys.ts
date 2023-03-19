c14f4dfa3238c3c745f56decf9e41209597afb97
Aleks Kožajev <aleks.kozajev@pipedrive.com>
2023-03-19 14:42:00 +0200

    renamed to ignoreEventWhen

--- src/useHotkeys.ts
+++ src/useHotkeys.ts
@@ -64,7 +64,7 @@ export default function useHotkeys<T extends HTMLElement>(
         return
       }
 
-      if (memoisedOptions?.ignoreEventCondition?.(e)) {
+      if (memoisedOptions?.ignoreEventWhen?.(e)) {
         return;
       }
 
